b22545ced870ed55af16c8d4433a72cdbfdd9065
apple <apple@apples-MacBook-Air.local>
2023-03-29 22:08:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portifolio-web-MS
---
+++ assets/scripts/changeDescription.js
@@ -0,0 +1,86 @@
+// Mudar a Descrição
+
+const changeDescription = (nameCard, text) => {
+    let infoSkills = document.querySelector('#info-skills')
+    let cardName = document.querySelector('.card-name')
+
+    document.querySelector(nameCard).addEventListener('mouseover', () => {
+        infoSkills.innerHTML = text
+        if(nameCard === '.html') {
+            cardName.innerHTML = ' Html '
+        } 
+        if(nameCard === '.css') {
+            cardName.innerHTML = ' Css '
+        }
+        if(nameCard === '.js') {
+            cardName.innerHTML = ' JavaScript '
+        }
+        if(nameCard === '.git') {
+            cardName.innerHTML = ' Git '
+        } 
+        if(nameCard === '.java') {
+            cardName.innerHTML = ' Java '
+        } 
+        if(nameCard === '.linux') {
+            cardName.innerHTML = ' Linux '
+        } 
+        if(nameCard === '.docker') {
+            cardName.innerHTML = ' Docker '
+        } 
+        
+        if(nameCard === '.sql') {
+            cardName.innerHTML = ' SQL '
+        }
+        if(nameCard === '.postman') {
+            cardName.innerHTML = ' Postman '
+        }  
+    })
+
+    document.querySelector(nameCard).addEventListener('mouseout', () => {
+        infoSkills.innerHTML = '* passe o cursor sobre os ícones para saber mais *'
+        cardName.innerHTML = ""
+    })
+}
+
+changeDescription(
+    '.html',
+    'Intermediário.'
+)
+
+changeDescription(
+    '.css',
+    'Intermediário.'
+)
+
+changeDescription(
+    '.js',
+    'Básico.'
+)
+
+changeDescription(
+    '.java',
+    'Básico.'   
+)
+changeDescription(
+    '.sql',
+    'Intermediário.'   
+)
+changeDescription(
+    '.git',
+    'Intermediário.'
+)
+changeDescription(
+    '.linux',
+    'Intermediário.'
+)
+
+changeDescription(
+    '.docker',
+    'Intermediário.'   
+)
+changeDescription(
+    '.postman',
+    'Básico.'
+)
+
+
